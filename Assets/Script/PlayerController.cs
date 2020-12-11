848d9c9a2d4128a8c41f606593b642d8196e3fed
Gdz.Cp-Pc <eryparasetyo.15@gmail.com>
2020-12-11 07:06:46 +0700

    Final

--- Assets/Script/PlayerController.cs
+++ Assets/Script/PlayerController.cs
@@ -1,5 +1,6 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using UnityEngine.SceneManagement;
 using UnityEngine;
 
 public class PlayerController : MonoBehaviour
@@ -75,5 +76,17 @@ public class PlayerController : MonoBehaviour
             }
         }
 
+    }
+
+    private void OnTriggerEnter(Collider trigger)
+    {
+        if (trigger.gameObject.tag == "Finsih")
+        {
+            SceneManager.LoadScene(2);
+        }
+        if (trigger.gameObject.tag == "Finsih2")
+        {
+            SceneManager.LoadScene(0);
+        }
     }
 }
---
+++ Assets/uiManager.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.SceneManagement;
+using UnityEngine;
+
+public class uiManager : MonoBehaviour
+{
+    public void PlayMe()
+    {
+        SceneManager.LoadScene(1);
+    }
+
+    public void Exit()
+    {
+        Application.Quit();
+    }
+
+    
+}
